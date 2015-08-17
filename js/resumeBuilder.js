9b1655e9ff15c78b5612a1c548a79e416da2f588
Mladen Kovacevic <mladen.kovacevic@gmail.com>
2015-08-16 23:19:55 -0400

    new file:   .idea/.name 	new file:   .idea/frontend-nanodegree-resume.iml 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/vcs.xml 	new file:   .idea/workspace.xml 	modified:   index.html 	new file:   js/bio.json 	new file:   js/education.json 	new file:   js/projects.json 	modified:   js/resumeBuilder.js 	new file:   js/work.json

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,66 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var name = "Mladen Kovacevic";
+var role = "Javascript Jedi";
+var contact = {
+    "email": "mladen.kovacevic@gmail.com",
+    "mobile": "416-906-1722",
+    "github": "http://www.github.com/gruntfarm",
+    "twitter": "@vrshift",
+    "location": "Toronto, Ontario"
+};
+var picture = "images/fry.jpg";
+var welcome = "Welcome to my resume!";
+var skills = ["Flying the X-Wing", "Lightsaber Fighting", "Using the Force", "Javascript"];
+
+var bio = {
+    "name": name,
+    "role": role,
+    "contact": contact,
+    "picture": picture,
+    "welcomeMessage": welcome,
+    "skills": skills
+};
+
+var work = {
+    "jobs": [
+        "employer":"",
+        "title":"",
+        "location":"",
+        "dates":"",
+        "description":"",
+    ]
+};
+
+var projects = {
+    "project": [
+        "title":"",
+        "dates":"",
+        "description":"",
+        "images":[""],
+    ]
+};
+
+var education = {
+    "schools": [
+        {
+            "name":"",
+            "city":"",
+            "degree":"",
+            "majors":[""],
+            "year":"",
+            "url":""
+        }
+    ]
+    "onlineCourses": [
+        {
+            "course": "",
+            "school":"",
+            "url":"",
+            "year":""
+
+        }
+    ]
+};
+
