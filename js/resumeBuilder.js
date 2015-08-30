3191429a9298f72b1816700a2727d2751b66aff5
Mladen Kovacevic <mladen.kovacevic@gmail.com>
2015-08-30 16:58:10 -0400

    modified:   js/bio.json 	modified:   js/education.json 	modified:   js/projects.json 	modified:   js/resumeBuilder.js 	modified:   js/work.json

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,66 +1,31 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-var name = "Mladen Kovacevic";
-var role = "Javascript Jedi";
-var contact = {
-    "email": "mladen.kovacevic@gmail.com",
-    "mobile": "416-906-1722",
-    "github": "http://www.github.com/gruntfarm",
-    "twitter": "@vrshift",
-    "location": "Toronto, Ontario"
-};
-var picture = "images/fry.jpg";
-var welcome = "Welcome to my resume!";
-var skills = ["Flying the X-Wing", "Lightsaber Fighting", "Using the Force", "Javascript"];
-
-var bio = {
-    "name": name,
-    "role": role,
-    "contact": contact,
-    "picture": picture,
-    "welcomeMessage": welcome,
-    "skills": skills
-};
-
 var work = {
     "jobs": [
-        "employer":"",
-        "title":"",
-        "location":"",
-        "dates":"",
-        "description":"",
-    ]
-};
-
-var projects = {
-    "project": [
-        "title":"",
-        "dates":"",
-        "description":"",
-        "images":[""],
-    ]
-};
-
-var education = {
-    "schools": [
         {
-            "name":"",
-            "city":"",
-            "degree":"",
-            "majors":[""],
-            "year":"",
-            "url":""
+            "employer": "Glacier Media Group",
+            "title": "Marketing manager",
+            "location": "Toronto, Ontario",
+            "dates": "2008-2015",
+            "description": "Revenue growth generation and customer nurturing achieved via careful planning, execution and analysis across a number of marketing channels. Lead in SEO optimization analysis and consulting (keyword and taxonomy strategy, back-linking expansion, reporting and guidance). Execution of all pay-per-click campaigns (Bing, Google, Facebook, Linkedin, Twitter). Development and maintenance of full e-mail marketing program reaching over 60K customers and prospects (list selection, segmentation, design, content curation, deployment and analysis ). Full-stack management of content marketing, landing pages and special promotions",
+        },
+        {
+            "employer": "Canamedia",
+            "title":"Technical Coordinator",
+            "location": "Toronto, Ontario",
+            "dates": "2006-2008",
+            "description": "Managed distribution and logistics of extensive library of international television content. Played a lead graphic designer and online marketing role",
         }
     ]
-    "onlineCourses": [
-        {
-            "course": "",
-            "school":"",
-            "url":"",
-            "year":""
+}
 
-        }
-    ]
-};
+for (job in work.jobs) {
+    $("#workExperience").append(HTMLworkStart);
+    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+    var formattedEmployerTitle = formattedEmployer + formattedTitle;
+    $(".work-entry:last").append(formattedEmployerTitle);
+
+}
 
